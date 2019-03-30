10094e0de9be77947a9e4798f59ba96e7d3192f6
John Gallagher <jgallagher@bignerdranch.com>
2019-03-30 16:36:43 -0400

    Add hello world examples

---
+++ examples/hello_world_consume.rs
@@ -0,0 +1,36 @@
+// Port of https://www.rabbitmq.com/tutorials/tutorial-one-python.html. Run this
+// in one shell, and run the hello_world_publish example in another.
+use amiquip::{Connection, ConsumerMessage, ConsumerOptions, QueueDeclareOptions, Result};
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    // Open connection.
+    let mut connection = Connection::open("amqp://guest:guest@localhost:5672")?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Declare the "hello" queue.
+    let queue = channel.queue_declare("hello", QueueDeclareOptions::default())?;
+
+    // Start a consumer.
+    let consumer = queue.consume(ConsumerOptions::default())?;
+    println!("Waiting for messages. Press Ctrl-C to exit.");
+
+    for (i, message) in consumer.receiver().iter().enumerate() {
+        match message {
+            ConsumerMessage::Delivery(delivery) => {
+                let body = String::from_utf8_lossy(&delivery.body);
+                println!("({:>3}) Received [{}]", i, body);
+                consumer.ack(delivery)?;
+            }
+            other => {
+                println!("Consumer ended: {:?}", other);
+                break;
+            }
+        }
+    }
+
+    connection.close()
+}
---
+++ examples/hello_world_publish.rs
@@ -0,0 +1,21 @@
+// Port of https://www.rabbitmq.com/tutorials/tutorial-one-python.html. Start the
+// hello_world_consume example in one shell, and run this in another.
+use amiquip::{Connection, Exchange, Publish, Result};
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    // Open connection.
+    let mut connection = Connection::open("amqp://guest:guest@localhost:5672")?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Get a handle to the direct exchange on our channel.
+    let exchange = Exchange::direct(&channel);
+
+    // Publish a message to the "hello" queue.
+    exchange.publish(Publish::new("hello there".as_bytes(), "hello"))?;
+
+    connection.close()
+}
