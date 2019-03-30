0b8d29a52414029f98cae6a10a5cd113981d4be9
John Gallagher <jgallagher@bignerdranch.com>
2019-03-29 21:16:25 -0400

    Add Get::{ack, nack, reject}.

--- src/get.rs
+++ src/get.rs
@@ -1,7 +1,24 @@
-use crate::Delivery;
+use crate::{Channel, Delivery, Result};
 
 #[derive(Clone, Debug)]
 pub struct Get {
     pub delivery: Delivery,
     pub message_count: u32,
+}
+
+impl Get {
+    #[inline]
+    pub fn ack(&self, channel: &Channel, multiple: bool) -> Result<()> {
+        self.delivery.ack(channel, multiple)
+    }
+
+    #[inline]
+    pub fn nack(&self, channel: &Channel, multiple: bool, requeue: bool) -> Result<()> {
+        self.delivery.nack(channel, multiple, requeue)
+    }
+
+    #[inline]
+    pub fn reject(&self, channel: &Channel, requeue: bool) -> Result<()> {
+        self.delivery.reject(channel, requeue)
+    }
 }
