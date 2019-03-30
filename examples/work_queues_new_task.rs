4b52717595ab2954548eb7fb0e224cff6edb7edb
John Gallagher <jgallagher@bignerdranch.com>
2019-03-30 19:36:42 -0400

    Add work_queues example.

---
+++ examples/work_queues_new_task.rs
@@ -0,0 +1,45 @@
+// Port of https://www.rabbitmq.com/tutorials/tutorial-two-python.html. Start one
+// or more work_queues_worker examples in other shells, then run this example.
+use amiquip::{AmqpProperties, Connection, Exchange, Publish, QueueDeclareOptions, Result};
+use std::env;
+
+const TASK_QUEUE: &'static str = "task_queue";
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    // Open connection.
+    let mut connection = Connection::open("amqp://guest:guest@localhost:5672")?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Declare the durable queue we are going to publish to, but discard the handle;
+    // we aren't going to do anything with it.
+    let _ = channel.queue_declare(
+        TASK_QUEUE,
+        QueueDeclareOptions {
+            durable: true,
+            ..QueueDeclareOptions::default()
+        },
+    )?;
+
+    // Get a handle to the direct exchange on our channel.
+    let exchange = Exchange::direct(&channel);
+
+    // Publish a message to the "hello" queue.
+    let mut message = env::args().skip(1).collect::<Vec<_>>().join(" ");
+    if message.is_empty() {
+        message = "Hello world.".to_string();
+    }
+
+    exchange.publish(Publish::with_properties(
+        message.as_bytes(),
+        TASK_QUEUE,
+        // delivery_mode 2 makes message persistent
+        AmqpProperties::default().with_delivery_mode(2),
+    ))?;
+    println!("Sent message [{}]", message);
+
+    connection.close()
+}
---
+++ examples/work_queues_worker.rs
@@ -0,0 +1,57 @@
+// Port of https://www.rabbitmq.com/tutorials/tutorial-two-python.html. Start one
+// or more of this example in shells, then run the work_queues_new_task example
+// in another.
+use amiquip::{Connection, ConsumerMessage, ConsumerOptions, QueueDeclareOptions, Result};
+use std::thread;
+use std::time::Duration;
+
+const TASK_QUEUE: &'static str = "task_queue";
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    // Open connection.
+    let mut connection = Connection::open("amqp://guest:guest@localhost:5672")?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Declare the durable queue we will consume from.
+    let queue = channel.queue_declare(
+        TASK_QUEUE,
+        QueueDeclareOptions {
+            durable: true,
+            ..QueueDeclareOptions::default()
+        },
+    )?;
+
+    // Set QOS to only send us 1 message at a time.
+    channel.qos(0, 1, false)?;
+
+    // Start a consumer.
+    let consumer = queue.consume(ConsumerOptions::default())?;
+    println!("Waiting for messages. Press Ctrl-C to exit.");
+
+    for (i, message) in consumer.receiver().iter().enumerate() {
+        match message {
+            ConsumerMessage::Delivery(delivery) => {
+                let body = String::from_utf8_lossy(&delivery.body);
+                println!("({:>3}) Received [{}]", i, body);
+
+                // Sleep for n seconds, where n is the number of '.' chars in the body,
+                // before we ack the message.
+                let dits = delivery.body.iter().filter(|&&b| b == b'.').count();
+                thread::sleep(Duration::from_secs(dits as u64));
+                println!("({:>3}) ... done sleeping", i);
+
+                consumer.ack(delivery)?;
+            }
+            other => {
+                println!("Consumer ended: {:?}", other);
+                break;
+            }
+        }
+    }
+
+    connection.close()
+}
