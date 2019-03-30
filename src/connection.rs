8c7acfdd495036dc0e4e0cf5603cffe411d0a337
John Gallagher <jgallagher@bignerdranch.com>
2019-03-29 20:56:15 -0400

    Document ConnectionTuning

--- src/connection.rs
+++ src/connection.rs
@@ -23,15 +23,54 @@ pub enum ConnectionBlockedNotification {
     Unblocked,
 }
 
+/// Tuning parameters for the amiquip client.
+///
+/// The options are solely used to control local behavior of the client. They are not part of the
+/// AMQP spec and are not communicated with the server in any way. For options that configure the
+/// AMQP connection, see [`ConnectionOptions`](struct.ConnectionOptions.html).
 pub struct ConnectionTuning {
+    /// Set the bound used when creating `mio_extras::channel::sync_channel()` channels for sending
+    /// messages to the connection's I/O thread. The default value for this field is 16.
+    ///
+    /// See the discussion on [amiquip's design details](index.html#design-details) for more
+    /// information.
     pub mem_channel_bound: usize,
+
+    /// Set the maximum amount of data in bytes that the I/O thread will buffer before it begins to
+    /// apply backpressure on clients by not reading from their channels. If the high water mark is
+    /// reached, the I/O loop will not resume reading from client channels until the amount of
+    /// buffered data drops below
+    /// [`buffered_writes_low_water`](struct.ConnectionTuning.html#structfield.buffered_writes_low_water)
+    /// bytes. The default value for this field is 16 MiB.
+    ///
+    /// See the discussion on [amiquip's design details](index.html#design-details) for more
+    /// information.
     pub buffered_writes_high_water: usize,
+
+    /// Set the low water mark for the I/O thread to resume reading from client channels. See
+    /// [`buffered_writes_high_water`](struct.ConnectionTuning.html#structfield.buffered_writes_high_water)
+    /// above. The default value for this field is 0 bytes.
     pub buffered_writes_low_water: usize,
+
+    /// Set the poll timeout to be used by the I/O thread. Note that setting this value can have
+    /// surprising behavior:
+    ///
+    /// * Internal messages to the I/O thread reset the poll timeout counter, so it is possible for
+    /// the underlying TCP connection to stop responding without ever triggering a poll timeout.
+    /// * If the poll timeout is set to a value less than the
+    /// [heartbeats](struct.ConnectionOptions.html#method.heartbeat) specified for the
+    /// connection, the poll timeout could fire (killing the I/O thread and ultimately connection)
+    /// even if the underlying connection is still healthy.
+    ///
+    /// The primary intention for this value is to allow the initial connection to the server to
+    /// time out. If you are not sure, leaving this value as `None` (the default) or setting it to
+    /// some value larger than twice the heartbeat option should be sufficient.
     pub poll_timeout: Option<Duration>,
 }
 
 impl Default for ConnectionTuning {
     fn default() -> Self {
+        // NOTE: If we change this, make sure to change the docs above.
         ConnectionTuning {
             mem_channel_bound: 16,
             buffered_writes_high_water: 16 << 20,
@@ -42,6 +81,7 @@ impl Default for ConnectionTuning {
 }
 
 impl ConnectionTuning {
+    /// Set the [channel memory bound](#structfield.mem_channel_bound).
     pub fn mem_channel_bound(self, mem_channel_bound: usize) -> Self {
         ConnectionTuning {
             mem_channel_bound,
@@ -49,6 +89,8 @@ impl ConnectionTuning {
         }
     }
 
+    /// Set the [high water mark](#structfield.buffered_writes_high_water) for buffered data to be
+    /// written to the underlying stream.
     pub fn buffered_writes_high_water(self, buffered_writes_high_water: usize) -> Self {
         ConnectionTuning {
             buffered_writes_high_water,
@@ -56,6 +98,8 @@ impl ConnectionTuning {
         }
     }
 
+    /// Set the [low water mark](#structfield.buffered_writes_low_water) for buffered data to be
+    /// written to the underlying stream.
     pub fn buffered_writes_low_water(self, buffered_writes_low_water: usize) -> Self {
         ConnectionTuning {
             buffered_writes_low_water,
@@ -63,6 +107,7 @@ impl ConnectionTuning {
         }
     }
 
+    /// Set the [poll timeout](#structfield.poll_timeout).
     pub fn poll_timeout(self, poll_timeout: Option<Duration>) -> Self {
         ConnectionTuning {
             poll_timeout,
