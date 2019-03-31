2de3d1c9ba5a778d07fe7f33390d7fca135a7d03
John Gallagher <jgallagher@bignerdranch.com>
2019-03-30 23:08:40 -0400

    Add pubsub example.

---
+++ examples/pubsub_emit_log.rs
@@ -0,0 +1,32 @@
+// Port of https://www.rabbitmq.com/tutorials/tutorial-three-python.html. Start one
+// or more pubsub_receive_logs examples in other shells, then run this example.
+use amiquip::{Connection, ExchangeDeclareOptions, ExchangeType, Publish, Result};
+use std::env;
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    // Open connection.
+    let mut connection = Connection::open("amqp://guest:guest@localhost:5672")?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Declare the fanout exchange we will publish to.
+    let exchange = channel.exchange_declare(
+        ExchangeType::Fanout,
+        "logs",
+        ExchangeDeclareOptions::default(),
+    )?;
+
+    // Publish a message to the logs queue.
+    let mut message = env::args().skip(1).collect::<Vec<_>>().join(" ");
+    if message.is_empty() {
+        message = "info: Hello world!".to_string();
+    }
+
+    exchange.publish(Publish::new(message.as_bytes(), ""))?;
+    println!("Sent [{}]", message);
+
+    connection.close()
+}
---
+++ examples/pubsub_receive_logs.rs
@@ -0,0 +1,59 @@
+// Port of https://www.rabbitmq.com/tutorials/tutorial-three-python.html. Start this
+// example in one shell, then run the pubsub_emit_log example in another.
+use amiquip::{
+    Connection, ConsumerMessage, ConsumerOptions, ExchangeDeclareOptions, ExchangeType, FieldTable,
+    QueueDeclareOptions, Result,
+};
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    // Open connection.
+    let mut connection = Connection::open("amqp://guest:guest@localhost:5672")?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Declare the fanout exchange we will bind to.
+    let exchange = channel.exchange_declare(
+        ExchangeType::Fanout,
+        "logs",
+        ExchangeDeclareOptions::default(),
+    )?;
+
+    // Declare the exclusive, server-named queue we will use to consume.
+    let queue = channel.queue_declare(
+        "",
+        QueueDeclareOptions {
+            exclusive: true,
+            ..QueueDeclareOptions::default()
+        },
+    )?;
+    println!("created exclusive queue {}", queue.name());
+
+    // Bind our queue to the logs exchange.
+    queue.bind(&exchange, "", FieldTable::new())?;
+
+    // Start a consumer. Use no_ack: true so the server doesn't wait for us to ack
+    // the messages it sends us.
+    let consumer = queue.consume(ConsumerOptions {
+        no_ack: true,
+        ..ConsumerOptions::default()
+    })?;
+    println!("Waiting for logs. Press Ctrl-C to exit.");
+
+    for (i, message) in consumer.receiver().iter().enumerate() {
+        match message {
+            ConsumerMessage::Delivery(delivery) => {
+                let body = String::from_utf8_lossy(&delivery.body);
+                println!("({:>3}) {}", i, body);
+            }
+            other => {
+                println!("Consumer ended: {:?}", other);
+                break;
+            }
+        }
+    }
+
+    connection.close()
+}
