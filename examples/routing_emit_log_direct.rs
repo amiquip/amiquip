d5f40722321f49b273f700adaf0103aeb6a94a2d
John Gallagher <jgallagher@bignerdranch.com>
2019-03-30 23:21:50 -0400

    Add routing examples

---
+++ examples/routing_emit_log_direct.rs
@@ -0,0 +1,34 @@
+// Port of https://www.rabbitmq.com/tutorials/tutorial-four-python.html. Start the
+// routing_receive_logs_direct example in another shell, then run this example.
+use amiquip::{Connection, ExchangeDeclareOptions, ExchangeType, Publish, Result};
+use std::env;
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    // Open connection.
+    let mut connection = Connection::open("amqp://guest:guest@localhost:5672")?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Declare the fanout exchange we will publish to.
+    let exchange = channel.exchange_declare(
+        ExchangeType::Direct,
+        "direct_logs",
+        ExchangeDeclareOptions::default(),
+    )?;
+
+    let mut args = env::args().skip(1);
+    let severity = args.next().unwrap_or_else(|| "info".to_string());
+
+    let mut message = args.collect::<Vec<_>>().join(" ");
+    if message.is_empty() {
+        message = "Hello world!".to_string();
+    }
+
+    exchange.publish(Publish::new(message.as_bytes(), severity.clone()))?;
+    println!("Sent {}:{}", severity, message);
+
+    connection.close()
+}
---
+++ examples/routing_receive_logs_direct.rs
@@ -0,0 +1,71 @@
+// Port of https://www.rabbitmq.com/tutorials/tutorial-four-python.html. Start this
+// example in one shell, then run the routing_emit_log_direct example in another.
+use amiquip::{
+    Connection, ConsumerMessage, ConsumerOptions, ExchangeDeclareOptions, ExchangeType, FieldTable,
+    QueueDeclareOptions, Result,
+};
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    // Open connection.
+    let mut connection = Connection::open("amqp://guest:guest@localhost:5672")?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Declare the fanout exchange we will bind to.
+    let exchange = channel.exchange_declare(
+        ExchangeType::Direct,
+        "direct_logs",
+        ExchangeDeclareOptions::default(),
+    )?;
+
+    // Declare the exclusive, server-named queue we will use to consume.
+    let queue = channel.queue_declare(
+        "",
+        QueueDeclareOptions {
+            exclusive: true,
+            ..QueueDeclareOptions::default()
+        },
+    )?;
+    println!("created exclusive queue {}", queue.name());
+
+    let mut args = std::env::args();
+    if args.len() < 2 {
+        eprintln!(
+            "usage: {} [info] [warning] [error]",
+            args.next()
+                .unwrap_or_else(|| "routing_receive_logs_direct".to_string())
+        );
+        std::process::exit(1);
+    }
+
+    for severity in args.skip(1) {
+        // Bind to each requested log severity.
+        queue.bind(&exchange, severity, FieldTable::new())?;
+    }
+
+    // Start a consumer. Use no_ack: true so the server doesn't wait for us to ack
+    // the messages it sends us.
+    let consumer = queue.consume(ConsumerOptions {
+        no_ack: true,
+        ..ConsumerOptions::default()
+    })?;
+    println!("Waiting for logs. Press Ctrl-C to exit.");
+
+    for (i, message) in consumer.receiver().iter().enumerate() {
+        match message {
+            ConsumerMessage::Delivery(delivery) => {
+                let body = String::from_utf8_lossy(&delivery.body);
+                println!("({:>3}) {}:{}", i, delivery.routing_key, body);
+            }
+            other => {
+                println!("Consumer ended: {:?}", other);
+                break;
+            }
+        }
+    }
+
+    connection.close()
+}
