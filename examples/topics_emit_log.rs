2f65e3d3d0180807634dac08c54e3c4f8a82a9c8
John Gallagher <jgallagher@bignerdranch.com>
2019-03-31 00:15:49 -0400

    Add topics example

---
+++ examples/topics_emit_log.rs
@@ -0,0 +1,34 @@
+// Port of https://www.rabbitmq.com/tutorials/tutorial-five-python.html. Start the
+// topics_receive_logs example in one shell, then run this example in another.
+use amiquip::{Connection, ExchangeDeclareOptions, ExchangeType, Publish, Result};
+use std::env;
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    // Open connection.
+    let mut connection = Connection::open("amqp://guest:guest@localhost:5672")?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Declare the exchange we will publish to.
+    let exchange = channel.exchange_declare(
+        ExchangeType::Topic,
+        "topic_logs",
+        ExchangeDeclareOptions::default(),
+    )?;
+
+    let mut args = env::args().skip(1);
+    let routing_key = args.next().unwrap_or_else(|| "anonymous.info".to_string());
+
+    let mut message = args.collect::<Vec<_>>().join(" ");
+    if message.is_empty() {
+        message = "Hello world!".to_string();
+    }
+
+    exchange.publish(Publish::new(message.as_bytes(), routing_key.clone()))?;
+    println!("Sent {}:{}", routing_key, message);
+
+    connection.close()
+}
---
+++ examples/topics_receive_logs.rs
@@ -0,0 +1,71 @@
+// Port of https://www.rabbitmq.com/tutorials/tutorial-five-python.html. Start this
+// example in one shell, then run the topics_emit_log example in another.
+use amiquip::{
+    Connection, ConsumerMessage, ConsumerOptions, ExchangeDeclareOptions, ExchangeType, FieldTable,
+    QueueDeclareOptions, Result,
+};
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    // Open connection.
+    let mut connection = Connection::open("amqp://guest:guest@localhost:5672")?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Declare the exchange we will bind to.
+    let exchange = channel.exchange_declare(
+        ExchangeType::Topic,
+        "topic_logs",
+        ExchangeDeclareOptions::default(),
+    )?;
+
+    // Declare the exclusive, server-named queue we will use to consume.
+    let queue = channel.queue_declare(
+        "",
+        QueueDeclareOptions {
+            exclusive: true,
+            ..QueueDeclareOptions::default()
+        },
+    )?;
+    println!("created exclusive queue {}", queue.name());
+
+    let mut args = std::env::args();
+    if args.len() < 2 {
+        eprintln!(
+            "usage: {} [binding_key]...",
+            args.next()
+                .unwrap_or_else(|| "topics_receive_logs".to_string())
+        );
+        std::process::exit(1);
+    }
+
+    for binding_key in args.skip(1) {
+        queue.bind(&exchange, binding_key, FieldTable::new())?;
+    }
+
+    // Start a consumer. Use no_ack: true so the server doesn't wait for us to ack
+    // the messages it sends us.
+    let consumer = queue.consume(ConsumerOptions {
+        no_ack: true,
+        ..ConsumerOptions::default()
+    })?;
+    println!("Waiting for logs. Press Ctrl-C to exit.");
+
+    for (i, message) in consumer.receiver().iter().enumerate() {
+        match message {
+            ConsumerMessage::Delivery(delivery) => {
+                let body = String::from_utf8_lossy(&delivery.body);
+                println!("({:>3}) {}:{}", i, delivery.routing_key, body);
+            }
+            other => {
+                println!("Consumer ended: {:?}", other);
+                break;
+            }
+        }
+    }
+
+    connection.close()
+}
+
