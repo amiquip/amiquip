e6aa330ec3a86d0974af3ade5d43fd932ae050c5
John Gallagher <jgallagher@bignerdranch.com>
2019-03-31 00:15:58 -0400

    Add RPC example

---
+++ examples/rpc_client.rs
@@ -0,0 +1,85 @@
+// Port of https://www.rabbitmq.com/tutorials/tutorial-six-python.html. Start the
+// rpc_server example in one shell, then run this example in another.
+use amiquip::{
+    AmqpProperties, Channel, Connection, Consumer, ConsumerMessage, ConsumerOptions, Exchange,
+    Publish, Queue, QueueDeclareOptions, Result,
+};
+use uuid::Uuid;
+
+struct FibonacciRpcClient<'a> {
+    exchange: Exchange<'a>,
+    queue: Queue<'a>,
+    consumer: Consumer<'a>,
+}
+
+impl<'a> FibonacciRpcClient<'a> {
+    fn new(channel: &Channel) -> Result<FibonacciRpcClient> {
+        let exchange = Exchange::direct(&channel);
+
+        let queue = channel.queue_declare(
+            "",
+            QueueDeclareOptions {
+                exclusive: true,
+                ..QueueDeclareOptions::default()
+            },
+        )?;
+        // Use channel.basic_consume() here instead of queue.consume() so the returned
+        // consumer is tied to the lifetime of the channel instead of the queue.
+        let consumer = channel.basic_consume(
+            queue.name(),
+            ConsumerOptions {
+                no_ack: true,
+                ..ConsumerOptions::default()
+            },
+        )?;
+
+        Ok(FibonacciRpcClient {
+            exchange,
+            queue,
+            consumer,
+        })
+    }
+
+    fn call(&self, n: u64) -> Result<String> {
+        let correlation_id = format!("{}", Uuid::new_v4());
+        self.exchange.publish(Publish::with_properties(
+            format!("{}", n).as_bytes(),
+            "rpc_queue",
+            AmqpProperties::default()
+                .with_reply_to(self.queue.name().to_string())
+                .with_correlation_id(correlation_id.clone()),
+        ))?;
+        for message in self.consumer.receiver().iter() {
+            match message {
+                ConsumerMessage::Delivery(delivery) => {
+                    if delivery.properties.correlation_id().as_ref() == Some(&correlation_id) {
+                        return Ok(String::from_utf8_lossy(&delivery.body).into());
+                    }
+                }
+                other => {
+                    println!("Consumer ended: {:?}", other);
+                    break;
+                }
+            }
+        }
+        // This should really be an Err(..), but we don't want to go through the trouble
+        // of defining a new error type for this example.
+        Ok("ERROR: server failed to respond to RPC call".to_string())
+    }
+}
+
+fn main() -> Result<()> {
+    // Open connection.
+    let mut connection = Connection::open("amqp://guest:guest@localhost:5672")?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    let rpc_client = FibonacciRpcClient::new(&channel)?;
+
+    println!("Requesting fib(30)");
+    let result = rpc_client.call(30)?;
+    println!("Got {}", result);
+
+    connection.close()
+}
---
+++ examples/rpc_server.rs
@@ -0,0 +1,73 @@
+// Port of https://www.rabbitmq.com/tutorials/tutorial-six-python.html. Start this
+// example in one shell, then the rpc_client example in another.
+use amiquip::{
+    AmqpProperties, Connection, ConsumerMessage, ConsumerOptions, Exchange, Publish,
+    QueueDeclareOptions, Result,
+};
+
+fn fib(n: u64) -> u64 {
+    match n {
+        0 => 0,
+        1 => 1,
+        n => fib(n - 1) + fib(n - 2),
+    }
+}
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    // Open connection.
+    let mut connection = Connection::open("amqp://guest:guest@localhost:5672")?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Get a handle to the default direct exchange.
+    let exchange = Exchange::direct(&channel);
+
+    // Declare the queue that will receive RPC requests.
+    let queue = channel.queue_declare("rpc_queue", QueueDeclareOptions::default())?;
+
+    // Start a consumer.
+    let consumer = queue.consume(ConsumerOptions::default())?;
+    println!("Awaiting RPC requests");
+
+    for (i, message) in consumer.receiver().iter().enumerate() {
+        match message {
+            ConsumerMessage::Delivery(delivery) => {
+                let body = String::from_utf8_lossy(&delivery.body);
+                println!("({:>3}) fib({})", i, body);
+
+                let (reply_to, corr_id) = match (
+                    delivery.properties.reply_to(),
+                    delivery.properties.correlation_id(),
+                ) {
+                    (Some(r), Some(c)) => (r.clone(), c.clone()),
+                    _ => {
+                        println!("received delivery without reply_to or correlation_id");
+                        consumer.ack(delivery)?;
+                        continue;
+                    }
+                };
+
+                let response = match body.parse() {
+                    Ok(n) => format!("{}", fib(n)),
+                    Err(_) => "invalid input".to_string(),
+                };
+
+                exchange.publish(Publish::with_properties(
+                    response.as_bytes(),
+                    reply_to,
+                    AmqpProperties::default().with_correlation_id(corr_id),
+                ))?;
+                consumer.ack(delivery)?;
+            }
+            other => {
+                println!("Consumer ended: {:?}", other);
+                break;
+            }
+        }
+    }
+
+    connection.close()
+}
