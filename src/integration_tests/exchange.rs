08f26da87c483f3fb964e2e95939a42c5e837bd7
John Gallagher <jgallagher@bignerdranch.com>
2019-04-09 20:29:04 -0400

    Add test and fix for exchange_declare_{nowait,passive}.
    Internal implementations were backwards.

--- src/integration_tests/exchange.rs
+++ src/integration_tests/exchange.rs
@@ -1,5 +1,5 @@
 use super::with_chan;
-use crate::Publish;
+use crate::{ExchangeDeclareOptions, ExchangeType, Publish};
 
 #[test]
 fn test_publish_empty() {
@@ -8,5 +8,36 @@ fn test_publish_empty() {
             chan.basic_publish("", Publish::new(&[], "does.not.exist"))
                 .unwrap();
         }
+    })
+}
+
+#[test]
+fn test_declare() {
+    let name = "amiquip-test-declare";
+
+    with_chan(|chan| {
+        let ex1 = chan
+            .exchange_declare(
+                ExchangeType::Direct,
+                name,
+                ExchangeDeclareOptions::default(),
+            )
+            .unwrap();
+        ex1.publish(Publish::new(&[], "does.not.exist")).unwrap();
+
+        let ex2 = chan
+            .exchange_declare_nowait(
+                ExchangeType::Direct,
+                name,
+                ExchangeDeclareOptions::default(),
+            )
+            .unwrap();
+        ex2.publish(Publish::new(&[], "does.not.exist")).unwrap();
+
+        let ex3 = chan.exchange_declare_passive(name).unwrap();
+        ex3.publish(Publish::new(&[], "does.not.exist")).unwrap();
+
+        assert_eq!(ex1.name(), ex2.name());
+        assert_eq!(ex1.name(), ex3.name());
     })
 }
